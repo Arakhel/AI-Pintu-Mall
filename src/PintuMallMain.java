bfb7f25f9d2f9d85c03bd689a9948fb26662a348
Nicolaus <euclides@zuragan.com>
2018-08-31 21:58:55 +0700

    Program Pintu Mall

--- src/PintuMallMain.java
+++ src/PintuMallMain.java
@@ -1,7 +1,7 @@
 public class PintuMallMain {
     public static void main(String[] args) {
         System.out.println("Ceritanya pintu otomatis mall");
-        PintuMall palang = new PintuMall();
+        new PintuMall();
         System.out.println();
         System.out.println("Sudah gak ada orang Bang");
     }
